0f774f98c41b17d701fc200b4b83d5d58a72aa08
seki <seki.ryohei0721@gmail.com>
2022-07-29 11:44:35 +0900

    ログイン画面と新規登録画面をbootstrap仕様に変更#

--- apps/config.py
+++ apps/config.py
@@ -7,6 +7,9 @@ class BaseConfig:
     SECRET_KEY = "2AZSMss3p5QPbcY2hBsJ"
     WTF_CSRF_SECRET_KEY = "AuwzyszU5sugKN7KZs6f"
 
+    # 画像アップロード先にapps/imagesを指定する
+    UPLOAD_FOLDER = str(Path(basedir, "apps", "images"))
+
 
 # LocalConfig クラスを作成する
 class LocalConfig(BaseConfig):
--- apps/detector/views.py
+++ apps/detector/views.py
@@ -1,8 +1,30 @@
-from flask import Blueprint, render_template, template_rendered
+from apps.app import db
+from apps.crud.models import User
+from apps.detector.models import UserImage
+from flask import (
+    Blueprint,
+    current_app,
+    render_template,
+    send_from_directory,
+    template_rendered,
+)
 
 dt = Blueprint("detector", __name__, template_folder="templates")
 
 
 @dt.route("/")
 def index():
-    return render_template("detector/index.html")
+
+    user_images = (
+        db.session.query(User, UserImage)
+        .join(UserImage)
+        .filter(User.id == UserImage.user_id)
+        .all()
+    )
+
+    return render_template("detector/index.html", user_images=user_images)
+
+
+@dt.route("/images/<path:filename>")
+def image_file(filename):
+    return send_from_directory(current_app.config["UPLOAD_FOLDER"], filename)
